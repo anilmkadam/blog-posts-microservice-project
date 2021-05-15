17cd17d3bd672dbf18cad22cd5f98c50bf1e3f98
Anil Kadam <anilkadam210@gmail.com>
2021-05-15 16:38:46 +0530

    feat: receive event code changes file

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsBypostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const postId = req.params.id;
   const commentId = randomBytes(4).toString("hex");
 
@@ -23,7 +24,23 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsBypostId[postId] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  });
+
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("event Received. ", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => {
