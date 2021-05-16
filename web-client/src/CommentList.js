8c729ee6a03fe1166b3b5b2d3b7d36473612e955
Anil Kadam <anilkadam210@gmail.com>
2021-05-16 22:17:20 +0530

    feat: Added comment updated logic at UI 🚀

--- web-client/src/CommentList.js
+++ web-client/src/CommentList.js
@@ -3,7 +3,19 @@ import React from "react";
 const CommentList = ({ comments }) => {
 
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    switch(comment.status) {
+      case "approved":
+        content = comment.content;
+      break;
+      case "pending": 
+        content = "This comment is awaiting Moderation.";
+      break;
+      case "rejected":
+        content = "This comment has been rejected.";
+      break;
+    }
+    return <li key={comment.id}>{content}</li>;
   });
   return <div>{renderedComments}</div>;
 };
